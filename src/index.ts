8276ffd5c65fc2b39913185b54787a53799b31e9
evanqian2175 <evanqian2175@gmail.com>
2022-12-08 18:16:55 +1300

    a

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { Client, Partials, GatewayIntentBits } from 'discord.js';
+const { Guilds, GuildMembers, MessageContent, GuildPresences, GuildMessages } =
+  GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const client = new Client({
+  intents: [
+    Guilds,
+    GuildMembers,
+    MessageContent,
+    GuildPresences,
+    GuildMessages,
+  ],
+  partials: [User, Message, GuildMember, ThreadMember],
+  failIfNotExists: false,
+  allowedMentions: { parse: ['users', 'roles', 'everyone'], repliedUser: true },
+});
+
+client.on('ready', async () => {
+  console.log('The bot is ready');
+});
+
+client.login(process.env.TOKEN);
