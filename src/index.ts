fae9c2beddf0187984a94a9b9695b874a728c92f
evanqian2175 <evanqian2175@gmail.com>
2022-12-12 19:36:46 +1300

    1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { config } from 'dotenv';
-config();
-import { Client, Partials, GatewayIntentBits, Collection } from 'discord.js';
+import 'dotenv/config'
+import { Client, Partials, GatewayIntentBits, Collection } from 'discord.js'
 const { Guilds, GuildMembers, MessageContent, GuildPresences, GuildMessages } =
-  GatewayIntentBits;
-const { User, Message, GuildMember, ThreadMember } = Partials;
+  GatewayIntentBits
+const { User, Message, GuildMember, ThreadMember } = Partials
+import { connect, connection } from 'mongoose'
+import chalk from 'chalk'
+const { TOKEN, MONGO_URI } = process.env
 
 const client = new Client({
   intents: [
@@ -16,10 +18,28 @@ const client = new Client({
   partials: [User, Message, GuildMember, ThreadMember],
   failIfNotExists: false,
   allowedMentions: { parse: ['users', 'roles', 'everyone'], repliedUser: true },
-});
+})
+
+const mongoStatus = ['disconnected', 'connected', 'connecting', 'disconnecting']
 
 client.on('ready', async () => {
-  console.log('The bot is ready');
-});
+  connect(MONGO_URI || ''),
+    console.log(
+      chalk.green(`[Database] MongoDB is ${mongoStatus[connection.readyState]}`)
+    )
+})
+;async () => {
+  const mongoStatus = [
+    'disconnected',
+    'connected',
+    'connecting',
+    'disconnecting',
+  ]
 
-client.login(process.env.TOKEN);
+  connect(MONGO_URI || ''),
+    console.log(
+      chalk.green(`[Database] MongoDB is ${mongoStatus[connection.readyState]}`)
+    )
+}
+
+client.login(TOKEN)
